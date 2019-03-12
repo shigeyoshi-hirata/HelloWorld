4e3af8ef25e345b14b77afe597784b19eb814f54
rintarou.okabe <regal-planning.dept@dance.ocn.ne.jp>
2019-03-12 14:49:53 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by MAC master on 2019/03/12.
+//  Copyright © 2019年 rintarou.okabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
